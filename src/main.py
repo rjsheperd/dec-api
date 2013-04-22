ae7202b71d74029bb189a98aac28cb619e45caa4
Adam Midvidy <amidvidy@gmail.com>
2013-04-21 18:07:01 -0700

    add mongo example

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-    # Python
+# Python
 import os
 import json
 import random
@@ -8,11 +8,16 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 
+# PyMongo
+import pymongo
+from bson import json_util
+
 class Application(tornado.web.Application):
 
     def __init__(self):
         handlers = [
             (r"/", HomeHandler),
+            (r"/user/", UserCreationHandler)
             ]
 
         settings = dict(
@@ -20,11 +25,28 @@ class Application(tornado.web.Application):
             static_path=os.path.join(os.path.dirname(__file__), "static"),
             debug=True,
         )
+        self.mongo = pymongo.Connection()
         tornado.web.Application.__init__(self, handlers, **settings)
 
 class HomeHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello World")
+
+class UserCreationHandler(tornado.web.RequestHandler):
+    def post(self):
+        # no error checking for now
+        first_name = self.get_argument('first_name')
+        last_name = self.get_argument('last_name')
+        skills = self.get_argument('skills')
+
+        user_collection = self.application.mongo.decapi.users
+
+        result = user_collection.insert({
+                'first_name': first_name,
+                'last_name': last_name,
+                'skills': skills
+                })
+        self.write(json.dumps(result, default=json_util.default))
 
 def main(port='8080', address='localhost'):
     http_server = tornado.httpserver.HTTPServer(Application())
